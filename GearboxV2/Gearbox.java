e1f9271b94e821a452cd43c8d09bd80b17c09994
RockFreakRickerd <57324086+RockFreakRickerd@users.noreply.github.com>
2022-03-14 09:22:26 +0100

    Gearbox update

--- GearboxV2/Gearbox.java
+++ GearboxV2/Gearbox.java
@@ -67,6 +67,12 @@ public class Gearbox{
   public Integer getNumberOfGears(){
     return this.numberOfGears;
   }
+  public Long getMaximumSpeed(){
+    return maxSpeeds.get(maxSpeeds.size()-1);
+  }
+  public List<Long> getSpeeds(){
+    return this.maxSpeeds;
+  }
   public void PrintInfo(){
     System.out.println("Gearbox `"+name+"` information.");
     System.out.println("Current gearbox type: "+type);
